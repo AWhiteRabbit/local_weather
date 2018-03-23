9591f2f34e43837dc1656c41dbe41f4bdfbb5bf2
AWhiteRabbit <doncameron09@gmail.com>
2018-03-23 16:37:32 -0400

    added json temp data

--- js/weather.js
+++ js/weather.js
@@ -8,9 +8,16 @@ $(".getWeather").click(function () {
             //get latitude and longitude
             var lat = pos.coords.latitude;
             var long = pos.coords.longitude;
-            var fccapi = "https://fcc-weather-api.glitch.me/api/current?lon="+long+"&lat="+lat+"";
-            console.log(fccapi);
-
+            var fccApiJson = "https://fcc-weather-api.glitch.me/api/current?lon="+long+"&lat="+lat+"";
+            // get JSON data from fccapi and use it
+            $.getJSON (fccApiJson, function (data) {
+                var temperature = data.main.temp;
+                var temperatureMin = data.main.temp_min;
+                var temperatureMax = data.main.temp_max;
+                $(".temp").html("temperature "+temperature);
+                $(".temp").append("<span>Min "+temperatureMin+"</span>");
+                $(".temp").append("<span>Max "+temperatureMax+"</span>");
+            })
         })
     } else {
         console.log("nope wut why?!?");
