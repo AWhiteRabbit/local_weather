1362845b9fc00e2e347ccf783b3b0f307e691909
AWhiteRabbit <don_cameron@hotmail.com>
2018-03-22 20:25:24 -0400

    got fcc api working correctly with navigatior.

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,18 @@
 
-var weather = "";
-var temp = 0;
-var location = "";
+//button to get geo location.
+$(".getWeather").click(function () {
+    console.log("clicky");
+    //check for geolocaion in browser
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            //get latitude and longitude
+            var lat = pos.coords.latitude;
+            var long = pos.coords.longitude;
+            var fccapi = "https://fcc-weather-api.glitch.me/api/current?lon="+long+"&lat="+lat+"";
+            console.log(fccapi);
 
-if ("geolocation" in navigator) {
-
-    navigator.geolocation.getCurrentPosition(function getLoc(pos) {
-        var lat = pos.coords.latitude;
-        var long = pos.coords.longitude;
-        var yourLoc = "https://fcc-weather-api.glitch.me/api/current?lon="+long+"&lat="+lat+"";
-
-    })
-}
+        })
+    } else {
+        console.log("nope wut why?!?");
+    }
+});
